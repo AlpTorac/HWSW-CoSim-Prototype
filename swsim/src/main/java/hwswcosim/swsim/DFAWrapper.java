bc14a5c8c7ffaf3593ba8207be3d29215f9fecee
AlpTorac <a.torac@hotmail.com>
2023-02-10 19:09:36 +0100

    Start implementing the software simulator

---
+++ swsim/src/main/java/hwswcosim/swsim/DFAWrapper.java
@@ -0,0 +1,65 @@
+package hwswcosim.swsim;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+
+import tel.schich.automata.DFA;
+import tel.schich.automata.State;
+import tel.schich.automata.transition.PlannedTransition;
+
+public class DFAWrapper {
+    private DFA dfa;
+    private Map<PlannedTransition, String> binaryMap;
+
+    private Collection<ScriptedTransitionEntry> transitionChain;
+    private ArrayList<ScriptedTransitionEntry> takenTransitions;
+
+    private State currentState;
+    private String currentBinaryPath;
+
+    public DFAWrapper(DFA dfa, Map<PlannedTransition, String> binaryMap, Collection<ScriptedTransitionEntry> transitionChain) {
+        this.dfa = dfa;
+        this.binaryMap = binaryMap;
+        this.currentState = this.dfa.getStartState();
+        this.transitionChain = transitionChain;
+        this.takenTransitions = new ArrayList<ScriptedTransitionEntry>();
+    }
+
+    public void step(long time) {
+        Optional<ScriptedTransitionEntry> transition = this.transitionChain.stream()
+            .filter(t -> {return t.time.doubleValue() == translateTime(time).doubleValue();})
+            .findFirst();
+
+        if (transition.isPresent()) {
+            ScriptedTransitionEntry t = transition.get();
+            Character input = t.input;
+
+            this.currentBinaryPath = this.binaryMap.get(this.dfa.getTransitionFor(this.currentState, input));
+
+            this.currentState = dfa.transition(this.currentState, input.charValue());
+            this.takenTransitions.add(t);
+        }
+    }
+
+    public String getCurrentBinaryPath() {
+        String binaryPath = this.currentBinaryPath;
+        this.currentBinaryPath = "";
+        return binaryPath;
+    }
+
+    protected Number translateTime(long time) {
+        return Double.valueOf(0);
+    }
+
+    protected class ScriptedTransitionEntry {
+        protected final Character input;
+        protected final Number time;
+
+        protected ScriptedTransitionEntry(Character input, Number time) {
+            this.input = input;
+            this.time = time;
+        }
+    }
+}
---
+++ swsim/src/main/java/hwswcosim/swsim/SoftwareSimulator.java
@@ -0,0 +1,141 @@
+package hwswcosim.swsim;
+
+import java.util.List;
+import java.util.Map;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import de.offis.mosaik.api.SimProcess;
+import de.offis.mosaik.api.Simulator;
+import tel.schich.automata.DFA;
+
+public class SoftwareSimulator extends Simulator {
+
+    private static final String DFAFilePathKeyName = "dfa_file_path";
+    private static final String binaryMapFilePathKeyName = "transition_to_binary_map_file_path";
+    private static final String transitionChainFilePathKeyName = "transition_chain_file_path";
+
+    private static final String binaryPathOutputName = "binary_file_path";
+
+    private static final String binaryExecutionStatsInputName = "binary_execution_stats";
+
+    private static final String stepSizeKeyName = "step_size";
+
+    private static final JSONObject meta = (JSONObject) JSONValue.parse(("{"
+            + "    'api_version': '" + Simulator.API_VERSION + "',"
+            + "    'type': 'time-based',"
+            + "    'models': {"
+            + "        'ExampleModel': {" + "            'public': true,"
+            + "            'params': ['init_val'],"
+            + "            'attrs': ['val', 'delta']" + "        }"
+            + "    }" + "}").replace("'", "\""));
+
+    public static void main(String[] args) throws Throwable {
+        Simulator sim = new SoftwareSimulator();
+        SimProcess.startSimulation(args, sim);
+    }
+
+    private int idCounter = 0;
+    private int stepSize = 1;
+    private final HashMap<String, DFAWrapper> instances;
+
+    public SoftwareSimulator() {
+        super("SoftwareSimulator");
+        this.instances = new HashMap<String, DFAWrapper>();
+    }
+
+    protected DFAWrapper parseDFAWrapper(String DFAFilePath, String binaryMapFilePath, String transitionChainFilePath) {
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Map<String, Object>> create(int num, String model, Map<String, Object> modelParams) throws Exception {
+        JSONArray entities = new JSONArray();
+
+        for (int i = 0; i < num; i++) {
+
+            String eid = "EM_" + (this.idCounter + i);
+
+            if (modelParams.containsKey(DFAFilePathKeyName) 
+            && modelParams.containsKey(binaryMapFilePathKeyName)
+            && modelParams.containsKey(transitionChainFilePathKeyName)) {
+                String DFAFilePath = (String) modelParams.get(DFAFilePathKeyName);
+                String binaryMapFilePath = (String) modelParams.get(binaryMapFilePathKeyName);
+                String transitionChainFilePath = (String) modelParams.get(transitionChainFilePathKeyName);
+
+                this.instances.put(eid, this.parseDFAWrapper(DFAFilePath, binaryMapFilePath, transitionChainFilePath));
+            } else {
+                continue;
+            }
+
+            JSONObject entity = new JSONObject();
+            entity.put("eid", eid);
+            entity.put("type", model);
+            entity.put("rel", new JSONArray());
+            entities.add(entity);
+        }
+        this.idCounter += num;
+        return entities;
+    }
+
+    @Override
+    public Map<String, Object> getData(Map<String, List<String>> outputs) throws Exception {
+        Map<String, Object> data = new HashMap<String, Object>();
+
+        for (Map.Entry<String, List<String>> entity : outputs.entrySet()) {
+            String eid = entity.getKey();
+            List<String> attrs = entity.getValue();
+            HashMap<String, Object> values = new HashMap<String, Object>();
+            DFAWrapper instance = this.instances.get(eid);
+
+            for (String attr : attrs) {
+                if (attr.equals(binaryPathOutputName)) {
+                	values.put(attr, instance.getCurrentBinaryPath());
+                }
+            }
+            data.put(eid, values);
+        }
+        return data;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Map<String, Object> init(String sid, Float timeResolution, Map<String, Object> simParams) throws Exception {
+        if (simParams.containsKey(stepSizeKeyName)) {
+            this.stepSize = ((Number) simParams.get(stepSizeKeyName)).intValue();
+        }
+        return SoftwareSimulator.meta;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public long step(long time, Map<String, Object> inputs, long maxAdvance) throws Exception {
+
+        for (Map.Entry<String, Object> entity : inputs.entrySet()) {
+            Map<String, Object> attrs = (Map<String, Object>) entity.getValue();
+
+            for (Map.Entry<String, Object> attr : attrs.entrySet()) {
+                String attrName = attr.getKey();
+                if (attrName.equals(binaryExecutionStatsInputName)) {
+                    // ToDo: Add binary execution stats to its aggregate
+                }
+                else {
+                	continue;
+                }
+            }
+        }
+
+        for (DFAWrapper instance : this.instances.values()) {
+            instance.step(time);
+        }
+
+        return time + this.stepSize;
+    }
+}
