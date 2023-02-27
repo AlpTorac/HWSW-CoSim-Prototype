fbeecd8900a05d8bb43fff5c8934dcd2cd4d40f0
AlpTorac <a.torac@hotmail.com>
2023-02-27 02:31:48 +0100

    Refactor evaluation_scenario.py and create class
    Extract evaluation_script
    
    Change binaryMap used in evaluation_scenario
    
    Add output directories to gitignore

--- evaluation_scenario.py
+++ evaluation_scenario.py
@@ -13,106 +13,194 @@ sys.path.append('./hwsim/evaluation')
 
 import evaluation_object
 
-start_time = evaluation_object.get_current_system_time()
+class EvaluationScenario():
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
 
-# Get the root directory of the project
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.sim_config = None
+        self.world_end = None
+        self.world = None
 
-# Gather all .jar files inside the project
-dependencies = ''
-swsim_jar_pattern = re.compile('swsim.*\.jar')
-swsim_jar_path = ''
-for root, dirnames, filenames in os.walk(ROOT_DIR):
-    for filename in fnmatch.filter(filenames, '*.jar'):
-        dependencies += ':' + os.path.join(root, filename)
-        if swsim_jar_pattern.match(filename) is not None:
-            swsim_jar_path = os.path.join(root, filename)
+        self.swsim = None
+        self.hwsim = None
 
-GEM5_PATH = ROOT_DIR+'/git-modules/gem5/build/X86/gem5.opt'
+        self.sw_model = None
+        self.hw_model = None
 
-# Run the main(...) methods of the mosaik APIs
-# with the dependencies gathered above
-SIM_CONFIG = {
-    'EvaluationSoftwareSimulator': {
-        'cmd': 'java -cp '+swsim_jar_path+dependencies+' hwswcosim.swsim.evaluation.EvaluationSoftwareSimulatorMosaikAPI %(addr)s',
-    },
-    'EvaluationHWSimulator': {
-        'cmd': '%(python)s ./hwsim/evaluation/evaluation_hardware_simulator_mosaik_API.py %(addr)s',
-    },
-}
-# End needs a buffer of at least 2 time steps, otherwise the software simulator
-# cannot receive its last input from the hardware simulator.
-# 
-# Receiving input is a part of the step() method and if the time it outputs
-# is >= END, step() will not be called again. Therefore, to ensure that step() is
-# called to receive the last input, one has to give it a buffer of at least 2
-# time steps.
-END = 9
+        self.root_dir = None
+        self.gem5_path = None
+        self.output_dir = None
+        self.swsim_output_dir = None
+        self.hwsim_output_dir = None
+        self.swsim_output_desc = None
 
-# Create World
-world = mosaik.World(SIM_CONFIG)
+        self.swsim_eval_output_file = None
+        self.hwsim_eval_output_file = None
+        self.eval_output_file = None
 
-OUTPUT_DIR = ROOT_DIR+'/out'
-SWSIM_OUTPUT_DIR = OUTPUT_DIR+'/swsimOut'
-HWSIM_OUTPUT_DIR = OUTPUT_DIR+'/hwsimOut'
+        self.resources_folder = None
 
-SWSIM_EVAL_OUTPUT_FILE = SWSIM_OUTPUT_DIR+'/swsimEvalOutput.txt'
-HWSIM_EVAL_OUTPUT_FILE = HWSIM_OUTPUT_DIR+'/hwsimEvalOutput.txt'
-EVAL_OUTPUT_FILE = OUTPUT_DIR+'/evalOutput.txt'
+        self.dfa_file_path = None
+        self.transition_to_binary_map_file_path = None
+        self.transition_chain_file_path = None
 
-# Start simulators
-software_simulator = world.start('EvaluationSoftwareSimulator',software_simulator_output_dir=SWSIM_OUTPUT_DIR
-        , software_simulator_eval_output_file_path=SWSIM_EVAL_OUTPUT_FILE
-        , software_simulator_output_desc={
-    'simSeconds': 'add',
-    'hostSeconds': 'add',
-    'simTicks': 'add',
-    'finalTick': 'add',
-    'hostMemory': 'add',
-    'simInsts': 'add',
-    'simOps': 'add',
-    'simFreq': 'none',
-    'hostTickRate': 'avg',
-    'hostInstRate': 'avg',
-    'hostOpRate': 'avg'
-})
-hardware_simulator = world.start('EvaluationHWSimulator'
-        , hardware_simulator_eval_output_file=HWSIM_EVAL_OUTPUT_FILE)
+        self.hardware_script_file_path = None
 
-RESOURCES_FOLDER = ROOT_DIR+'/gem5-scenario-resources'
+    def run_simulation(self):
+        self.start_evaluation_time()
 
-# Instantiate models
-sw_model = software_simulator.DFAWrapper(
-    dfa_file_path=RESOURCES_FOLDER+'/dfa.json',
-    transition_to_binary_map_file_path=RESOURCES_FOLDER+'/binaryMap.json',
-    transition_chain_file_path=RESOURCES_FOLDER+'/transitionChain.json')
+        self.create_mosaik_world()
+        self.start_software_simulator()
+        self.start_hardware_simulator()
+        self.init_software_model()
+        self.init_hardware_model()
+        self.connect_models()
+        self.run_simulation()
+        
+        self.end_simulation_time()
 
-hw_model = hardware_simulator.HWModel(
-    gem5_run_command=GEM5_PATH,
-    gem5_output_path=HWSIM_OUTPUT_DIR,
-    hardware_script_run_command=ROOT_DIR+'/hwsim/hardware_script.py')
+    def start_evaluation_time(self):
+        self.start_time = evaluation_object.get_current_system_time()
 
-world.connect(sw_model, hw_model, 'binary_file_path_out', 'binary_file_path_in')
-world.connect(sw_model, hw_model, 'binary_file_arguments_out', 'binary_file_arguments_in')
-world.connect(hw_model, sw_model, 'binary_execution_stats_out', 'binary_execution_stats_in', weak=True)
+    def init_root_dir_path(self):
+        self.root_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Run simulation
-world.run(until=END)
+    def init_gem5_path(self, relative_gem5_path='git-modules/gem5/build/X86/gem5.opt'):
+        self.gem5_path = self.root_dir+'/'+relative_gem5_path
+    
+    """
+    End needs a buffer of at least 2 time steps, otherwise the software simulator
+    cannot receive its last input from the hardware simulator. Hence the
+    ending_time_step + 2 in the method.
 
-end_time = evaluation_object.get_current_system_time()
+    Receiving input is a part of the step() method and if the time it outputs
+    is >= END, step() will not be called again. Therefore, to ensure that step() is
+    called to receive the last input, one has to give it a buffer of at least 2
+    time steps.
 
-eval_output = open(EVAL_OUTPUT_FILE, 'x')
-swsim_eval_output = open(SWSIM_EVAL_OUTPUT_FILE)
-hwsim_eval_output = open(HWSIM_EVAL_OUTPUT_FILE)
+    last_time_step should be the equal to the time of the last transition in
+    the transition chain.
+    """
+    def init_world_end(self, last_time_step):
+        self.world_end = last_time_step + 2
 
-eval_output.write('swsim evaluation outputs:\n')
-eval_output.writelines(swsim_eval_output.readlines())
-eval_output.write('\n')
-eval_output.write('hwsim evaluation outputs:\n')
-eval_output.writelines(hwsim_eval_output.readlines())
-eval_output.write('\n')
-eval_output.write('Entire simulation took: %.0f' % (end_time - start_time))
+    def init_sim_config(self):
+        # Gather all .jar files inside the project
+        dependencies = ''
+        swsim_jar_pattern = re.compile('swsim.*\.jar')
+        swsim_jar_path = ''
+        for root, dirnames, filenames in os.walk(self.root_dir):
+            for filename in fnmatch.filter(filenames, '*.jar'):
+                dependencies += ':' + os.path.join(root, filename)
+                if swsim_jar_pattern.match(filename) is not None:
+                    swsim_jar_path = os.path.join(root, filename)
 
-eval_output.close()
-swsim_eval_output.close()
-hwsim_eval_output.close()
+        self.sim_config = {
+            'EvaluationSoftwareSimulator': {
+                'cmd': 'java -cp '+swsim_jar_path+dependencies+' hwswcosim.swsim.evaluation.EvaluationSoftwareSimulatorMosaikAPI %(addr)s',
+            },
+            'EvaluationHWSimulator': {
+                'cmd': '%(python)s ./hwsim/evaluation/evaluation_hardware_simulator_mosaik_API.py %(addr)s',
+            },
+        }
+    
+    def create_mosaik_world(self):
+        self.world = mosaik.World(self.sim_config)
+
+    def init_output_dir_path(self, dir_name='out'):
+        self.output_dir = self.root_dir+'/'+dir_name
+    
+    def init_swsim_output_dir_path(self, dir_name='swsimOut'):
+        self.swsim_output_dir = self.output_dir+'/'+dir_name
+
+    def init_hwsim_output_dir_path(self, dir_name="hwsimOut"):
+        self.hwsim_output_dir = self.output_dir+'/'+dir_name
+    
+    def init_swsim_eval_output_file_path(self, file_name='swsimEvalOutput.txt'):
+        self.swsim_eval_output_file = self.swsim_output_dir+'/'+file_name
+    
+    def init_hwsim_eval_output_file_path(self, file_name='hwsimEvalOutput.txt'):
+        self.hwsim_eval_output_file = self.hwsim_output_dir+'/'+file_name
+    
+    def init_eval_output_file_path(self, file_name='evalOutput.txt'):
+        self.eval_output_file = self.output_dir+'/'+file_name
+
+    def init_swsim_output_description(self):
+        self.swsim_output_desc = {
+                'simSeconds': 'add',
+                'hostSeconds': 'add',
+                'simTicks': 'add',
+                'finalTick': 'add',
+                'hostMemory': 'add',
+                'simInsts': 'add',
+                'simOps': 'add',
+                'simFreq': 'none',
+                'hostTickRate': 'avg',
+                'hostInstRate': 'avg',
+                'hostOpRate': 'avg'
+            }
+
+    def start_software_simulator(self):
+        self.swsim = self.world.start('EvaluationSoftwareSimulator',software_simulator_output_dir=self.swsim_output_dir
+            , software_simulator_eval_output_file_path=self.swsim_eval_output_file
+            , software_simulator_output_desc=self.swsim_output_desc)
+    
+    def start_hardware_simulator(self):
+        self.hwsim = self.world.start('EvaluationHWSimulator'
+            , hardware_simulator_eval_output_file=self.hwsim_eval_output_file)
+
+    def init_resources_dir_path(self, relative_path):
+        self.resources_folder = self.root_dir+'/'+relative_path
+    
+    def init_dfa_file_path(self, file_name='dfa.json'):
+        self.dfa_file_path = self.resources_folder+'/'+file_name
+
+    def init_transition_to_binary_map_file_path(self, file_name='binaryMap.json'):
+        self.transition_to_binary_map_file_path = self.resources_folder+'/'+file_name
+    
+    def init_transition_chain_file_path(self, file_name='transitionChain.json'):
+        self.transition_chain_file_path = self.resources_folder+'/'+file_name
+
+    def init_software_model(self):
+        self.sw_model = self.swsim.DFAWrapper(
+            dfa_file_path=self.dfa_file_path,
+            transition_to_binary_map_file_path=self.transition_to_binary_map_file_path,
+            transition_chain_file_path=self.transition_chain_file_path)
+
+    def init_hardware_script_file_path(self, relative_path='hwsim/hardware_script.py'):
+        self.hardware_script_file_path = self.root_dir+'/'+relative_path
+
+    def init_hardware_model(self):
+        self.hw_model = self.hwsim.HWModel(
+            gem5_run_command=self.gem5_path,
+            gem5_output_path=self.hwsim_output_dir,
+            hardware_script_run_command=self.hardware_script_file_path)
+
+    def connect_models(self):
+        self.world.connect(self.sw_model, self.hw_model, 'binary_file_path_out', 'binary_file_path_in')
+        self.world.connect(self.sw_model, self.hw_model, 'binary_file_arguments_out', 'binary_file_arguments_in')
+        self.world.connect(self.hw_model, self.sw_model, 'binary_execution_stats_out', 'binary_execution_stats_in', weak=True)
+
+    def run_simulation(self):
+        self.world.run(until=self.world_end)
+
+    def end_simulation_time(self):
+        self.end_time = evaluation_object.get_current_system_time()
+
+    def write_evaluation_output(self):
+        eval_output = open(self.eval_output_file, 'x')
+        swsim_eval_output = open(self.swsim_eval_output_file)
+        hwsim_eval_output = open(self.hwsim_eval_output_file)
+
+        eval_output.write('swsim evaluation outputs:\n')
+        eval_output.writelines(swsim_eval_output.readlines())
+        eval_output.write('\n')
+        eval_output.write('hwsim evaluation outputs:\n')
+        eval_output.writelines(hwsim_eval_output.readlines())
+        eval_output.write('\n')
+        eval_output.write('Entire simulation took: %.0f' % (self.end_time - self.start_time))
+
+        eval_output.close()
+        swsim_eval_output.close()
+        hwsim_eval_output.close()
---
+++ evaluation_script.py
@@ -0,0 +1,82 @@
+import sys
+import evaluation_scenario
+import evaluation_object
+import os
+
+eval_folder_name = 'eval'
+
+if os.path.exists(os.path.dirname(os.path.abspath(__file__))+'/'+eval_folder_name):
+    raise Exception('The evaluation cannot run, if an evaluation output folder already exists')
+
+# Argument 1 = How many times the binary will run on WSL
+number_of_binary_runs = int(sys.argv[1])
+# Argument 2 = How many times the co-simulation evaluation will run
+number_of_eval_runs = int(sys.argv[2])
+
+avg_binary_time = 0
+binary_with_args = 'ackermann 2 2000'
+# Run the binary
+for x in range(number_of_binary_runs):
+    start_time = evaluation_object.get_current_system_time()
+    os.system('./evaluation-scenario-resources/'+binary_with_args)
+    end_time = evaluation_object.get_current_system_time()
+    avg_binary_time += end_time - start_time
+
+print('\n Average runtime for '+binary_with_args+' is: ' + str((avg_binary_time/number_of_binary_runs) / 1000000000) + '\n')
+
+# ackermann(2, 2000) ~ 0.02s
+# ackermann(2, 4000) ~ 0.076s
+# ackermann(2, 4500) ~ 0.093s
+# ackermann(2, 5000) ~ 0.114s
+# ackermann(2, 10000) ~ 0.45s
+# ackermann(2, 11000) ~ 0.54s
+# ackermann(2, 12000) ~ 0.64s
+# ackermann(2, 14000) ~ 0.88s
+# ackermann(2, 15000) ~ 1s
+# ackermann(2, 20000) ~ 1.8s
+# ackermann(2, 21000) ~ 2s
+# ackermann(2, 25000) ~ 2.8s
+
+# co-simulation time ~ 1000 x binary runs on WSL
+
+# Run the co-simulation evaluation
+for x in range(number_of_eval_runs):
+    eval_scenario = evaluation_scenario.EvaluationScenario()
+
+    eval_scenario.start_evaluation_time()
+
+    eval_scenario.init_root_dir_path()
+    eval_scenario.init_gem5_path()
+
+    eval_scenario.init_output_dir_path(eval_folder_name+'/out'+str(x))
+    eval_scenario.init_resources_dir_path('evaluation-scenario-resources')
+
+    eval_scenario.init_eval_output_file_path()
+
+    eval_scenario.init_swsim_output_dir_path()
+    eval_scenario.init_dfa_file_path()
+    eval_scenario.init_transition_to_binary_map_file_path()
+    eval_scenario.init_transition_chain_file_path()
+
+    eval_scenario.init_swsim_eval_output_file_path()
+    eval_scenario.init_swsim_output_description()
+
+    eval_scenario.init_hwsim_output_dir_path()
+    eval_scenario.init_hardware_script_file_path()
+
+    eval_scenario.init_hwsim_eval_output_file_path()
+
+    eval_scenario.init_sim_config()
+    eval_scenario.init_world_end(7)
+
+    eval_scenario.create_mosaik_world()
+    eval_scenario.start_software_simulator()
+    eval_scenario.start_hardware_simulator()
+    eval_scenario.init_software_model()
+    eval_scenario.init_hardware_model()
+    eval_scenario.connect_models()
+    eval_scenario.run_simulation()
+
+    eval_scenario.end_simulation_time()
+
+    eval_scenario.write_evaluation_output()
